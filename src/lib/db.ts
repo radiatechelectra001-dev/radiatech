1593fd29ae9184492d15df9904edef35cc8167c2
Rishu <daarogaakumar@gmail.com>
2026-04-30 11:37:06 +0530

    fix: add ssl rejectUnauthorized false for Supabase pooler TLS

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -10,7 +10,7 @@ function createPrismaClient() {
 		throw new Error("DATABASE_URL is required");
 	}
 
-	const adapter = new PrismaPg({ connectionString });
+	const adapter = new PrismaPg({ connectionString, ssl: { rejectUnauthorized: false } });
 	return new PrismaClient({ adapter });
 }
 
